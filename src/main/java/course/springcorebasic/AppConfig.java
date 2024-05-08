84d815c16163957b1fdb60e82de35723f8ae89ba
Jisu Lee <jisulee.shsf@gmail.com>
2024-05-09 00:59:42 +0900

    AppConfig 리팩터링

--- src/main/java/course/springcorebasic/AppConfig.java
+++ src/main/java/course/springcorebasic/AppConfig.java
@@ -1,6 +1,8 @@
 package course.springcorebasic;
 
+import course.springcorebasic.discount.DiscountPolicy;
 import course.springcorebasic.discount.FixDiscountPolicy;
+import course.springcorebasic.member.MemberRepository;
 import course.springcorebasic.member.MemberService;
 import course.springcorebasic.member.MemberServiceImpl;
 import course.springcorebasic.member.MemoryMemberRepository;
@@ -8,11 +10,20 @@ import course.springcorebasic.order.OrderService;
 import course.springcorebasic.order.OrderServiceImpl;
 
 public class AppConfig {
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
