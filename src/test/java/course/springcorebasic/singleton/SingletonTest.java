d2f7c21ef4ac862f2604b090c5b5f500a1998118
Jisu Lee <jisulee.shsf@gmail.com>
2024-05-09 15:40:51 +0900

    스프링 DI 컨테이너 사용에 따른 싱글톤 방식 동작 확인

--- src/test/java/course/springcorebasic/singleton/SingletonTest.java
+++ src/test/java/course/springcorebasic/singleton/SingletonTest.java
@@ -4,13 +4,15 @@ import course.springcorebasic.AppConfig;
 import course.springcorebasic.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonTest {
 
     @Test
-    @DisplayName("Spring이 없는 순수한 DI 컨테이너")
+    @DisplayName("순수 DI 컨테이너 사용")
     void pureContainer() {
         AppConfig appConfig = new AppConfig();
         MemberService memberService1 = appConfig.memberService();
@@ -19,10 +21,19 @@ public class SingletonTest {
     }
 
     @Test
-    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    @DisplayName("싱글톤 패턴 적용")
     void singletonServiceTest() {
         SingletonService singletonService1 = SingletonService.getInstance();
         SingletonService singletonService2 = SingletonService.getInstance();
         assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 DI 컨테이너 사용 -> 싱글톤 방식으로 동작 확인")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
