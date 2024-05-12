bd210c783c60c2a217daded11e47aa08c7a8e228
Jisu Lee <jisulee.shsf@gmail.com>
2024-05-13 00:05:30 +0900

    InitializingBean 및 DisposableBean 인터페이스 사용

--- src/test/java/course/springcorebasic/lifecycle/NetworkClient.java
+++ src/test/java/course/springcorebasic/lifecycle/NetworkClient.java
@@ -1,14 +1,14 @@
 package course.springcorebasic.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출 url = " + url);
-        connect();
-        call("초기화 연결 메시지");
-        disconnect();
     }
 
     public void setUrl(String url) {
@@ -25,5 +25,18 @@ public class NetworkClient {
 
     public void disconnect() {
         System.out.println("close = " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
