4e649c4eee1ab6ea409b6fb5d71ad1474faafaeb
Jisu Lee <jisulee.shsf@gmail.com>
2024-05-24 23:38:41 +0900

    싱글톤 및 프로토타입 스코프 비교

---
+++ src/test/java/course/springcorebasic/scope/SingletonTest.java
@@ -0,0 +1,36 @@
+package course.springcorebasic.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        Object singletonBean1 = ac.getBean(SingletonBean.class);
+        Object singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+    static class SingletonBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
