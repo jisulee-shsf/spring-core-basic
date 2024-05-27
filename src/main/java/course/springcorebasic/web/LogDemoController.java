f40723514cd67b7c533e2744619895ab81279366
Jisu Lee <jisulee.shsf@gmail.com>
2024-05-27 21:33:57 +0900

    프록시를 통한 문제 해결

--- src/main/java/course/springcorebasic/web/LogDemoController.java
+++ src/main/java/course/springcorebasic/web/LogDemoController.java
@@ -3,7 +3,6 @@ package course.springcorebasic.web;
 import course.springcorebasic.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,14 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
     private final LogDemoService logDemoService;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
 
